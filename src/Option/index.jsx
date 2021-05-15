0dca35641573f1503ac6763d791fbee1abfa791c
traja-traja <traja@traja.cz>
2021-05-15 16:35:18 +0200

    reseni-2

--- src/Option/index.jsx
+++ src/Option/index.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Icon from '../Icon/index';
 import './style.css';
 
-const Option = (props) => (
-  <div className="option">
-    <Icon type={props.type} />
-    <div>{props.text}</div>
-  </div>
-);
+const Option = (props) => {
+  const handleClick = () => {
+    console.log('kliknuto');
+    props.onSelected();
+  };
+
+  return (
+    <div className="option" onClick={handleClick}>
+      <Icon type={props.type} />
+      <div>{props.text}</div>
+    </div>
+  );
+};
 
 export default Option;
--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
 
-const Question = (props) => (
-  <div className="question">
-    <QuestionBody 
-      iconType="symbolQuestion" 
-      text="Kdo jinému jámu kopá, sám do ní padá"
-    />
-    <div className="question__options">
-      <Option type="smileyStrongYes" text="Souhlasím"/>
-      <Option type="smileyYes" text="Spíše souhlasím"/>
-      <Option type="smileyNeutral" text="Nevím"/>
-      <Option type="smileyNo" text="Spíše nesouhlasím"/>
-      <Option type="smileyStrongNo" text="Nesouhlasím"/>
+const Question = (props) => {
+  const [answered, setAnswered] = useState(false);
+  
+  const handleSelectedAnswer = () => {
+    console.log('selected answer');
+    setAnswered(true);
+
+  };
+
+  return (
+    <div className="question">
+      <QuestionBody 
+        iconType={answered ? 'symbolTick' : 'symbolQuestion'}
+        text="Kdo jinému jámu kopá, sám do ní padá"
+      />
+      <div className="question__options">
+        <Option onSelected={handleSelectedAnswer} onSelected={handleSelectedAnswer} type="smileyStrongYes" text="Souhlasím"/>
+        <Option onSelected={handleSelectedAnswer} type="smileyYes" text="Spíše souhlasím"/>
+        <Option onSelected={handleSelectedAnswer} type="smileyNeutral" text="Nevím"/>
+        <Option onSelected={handleSelectedAnswer} type="smileyNo" text="Spíše nesouhlasím"/>
+        <Option onSelected={handleSelectedAnswer} type="smileyStrongNo" text="Nesouhlasím"/>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Question;
